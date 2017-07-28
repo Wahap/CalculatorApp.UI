66b4a2c721b85d2f9ec67133fc4978cee8adeccd
Abdulvahap Demir <demir.vahap@gmail.com>
2017-07-28 14:24:35 +0100

    npm install bootstrap - dont forget to install jquery

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,6 +8,7 @@ import { ConfigService, IConfig } from '../app.config';
 })
 export class LoginComponent implements OnInit {
   model: any;
+  
   constructor() {
 
     this.model = { email: 'EMAIL212223', password: '123' }
@@ -23,7 +24,9 @@ export class LoginComponent implements OnInit {
 
   clickButton() {
     this.model.email = "wapper";
-    console.log(this.model.email);
+     
+    //console.log(this.model.email);
+   // alert("yeah");
   }
 
 }
